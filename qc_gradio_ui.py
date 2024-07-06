37b7d70e1ccd842fa7ba16442b51a1229daf2610
Brent Benson <125278138+bbenson-hbs@users.noreply.github.com>
2024-07-06 12:19:53 -0400

    Added source/context to Gradio UI

--- qc_gradio_ui.py
+++ qc_gradio_ui.py
@@ -13,21 +13,37 @@ query_chain = conversational_retrieval_chain(
     connection_string=os.getenv("CONNECTION_STRING"),
 )
 
-def query_model(message, history):
+def format_chat_history(history):
+    """Format the message history from the `Chatbot`
+    component (a list of string pairs) for LangChain."""
     chat_history = InMemoryChatMessageHistory()
     for human_msg, ai_msg in history:
         chat_history.add_user_message(human_msg)
         chat_history.add_ai_message(ai_msg)
-    partial_response = ""
+    return chat_history
+
+def query_model(message, history):
+    chat_history = format_chat_history(history)
     response_generator = query_chain.stream({
         "question": message,
         "chat_history": chat_history.messages,
     })
+    partial_response = ""
+    context = []
     for resp_part in response_generator:
-        if "answer" in resp_part:
+        if "context" in resp_part:
+            # Keep track of RAG context for later streaming
+            context = resp_part["context"]
+        elif "answer" in resp_part:
+            "Build and yield reasponse as chunks are streamed."
             partial_response = (
                 partial_response + resp_part["answer"].content
             )
+            yield partial_response
+    # Stream RAG context saved above
+    for doc in context:
+        label = doc.metadata["label"]
+        partial_response = partial_response + f"\n- {label}"
         yield partial_response
 
 gr.ChatInterface(query_model).launch()
